cf84ce04d18a8cc476b7d06daebfb46af92d5688
Naveen <naveen.chaurasia861@gmail.com>
2022-12-25 11:31:24 +0530

    added Outhss02

--- src/main/java/com/naveen/facebookLogin/FacebookLoginApplication2.java
+++ src/main/java/com/naveen/facebookLogin/FacebookLoginApplication2.java
@@ -2,12 +2,15 @@ package com.naveen.facebookLogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
 
 @SpringBootApplication
-public class FacebookLoginApplication {
+@EnableOAuth2Sso
+public class FacebookLoginApplication2 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FacebookLoginApplication.class, args);
+		SpringApplication.run(FacebookLoginApplication2.class, args);
 	}
 
 }
